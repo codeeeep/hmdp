d418c1a93527b1f20c5569585928af3b17fc7ad3
程瑜 <2479676487@qq.com>
2023-07-29 10:04:57 +0800

    🐛 Fix(UserHolder): ThreadLocal util returns UserDTO->User

---
+++ src/main/java/com/hmdp/config/MvcConfig.java
@@ -0,0 +1,7 @@
+package com.hmdp.config;/**
+ *@author codeep
+ *@date 2023/7/29 9:14
+ *@description:
+ */
+public class MvcConfig {
+}
---
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -0,0 +1,9 @@
+package com.hmdp.utils;
+
+/**
+ * @author codeep
+ * @date 2023/7/29 8:55
+ * @description:
+ */
+public class LoginIntercepter {
+}
