91f27bdf3f817b0df6dd8e9c85afda281914b43d
程瑜 <2479676487@qq.com>
2023-07-29 10:12:09 +0800

    Feat(LoginInterceptor): Add LoginInterceptor to check login status

--- src/main/java/com/hmdp/config/MvcConfig.java
+++ src/main/java/com/hmdp/config/MvcConfig.java
@@ -1,7 +1,31 @@
-package com.hmdp.config;/**
- *@author codeep
- *@date 2023/7/29 9:14
- *@description:
+package com.hmdp.config;
+
+import com.hmdp.utils.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author codeep
+ * @date 2023/7/29 9:14
+ * @description: 拦截器生效配置，配置拦截器拦截路径
  */
-public class MvcConfig {
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor())
+                .excludePathPatterns(
+                        "/user/login",
+                        "/blog/hot",
+                        "/user/code",
+                        "/shop/**",
+                        "/shop-type/**",
+                        "/upload/**",
+                        "/voucher/**"
+                );
+    }
 }
--- src/main/java/com/hmdp/utils/LoginInterceptor.java
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -1,9 +1,42 @@
 package com.hmdp.utils;
 
+import com.hmdp.dto.UserDTO;
+import com.hmdp.entity.User;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 /**
  * @author codeep
  * @date 2023/7/29 8:55
- * @description:
+ * @description: 登录拦截器
  */
-public class LoginIntercepter {
+public class LoginInterceptor implements HandlerInterceptor {
+
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // 1. 获取 session
+        HttpSession session = request.getSession();
+        // 2. 获取用户信息
+        Object user = session.getAttribute("user");
+        // 3. 判断用户是否存在
+        if (user == null) {
+            // 4. 不存在则拦截, 前端友好返回 401 状态码（具体的401错误是指:未授权,请求要求进行身份验证）
+            response.setStatus(401);
+            return false;
+        }
+        // 5. 存在则保存用户信息到 ThreadLocal 中
+        UserHolder.saveUser((User) user);
+        // 6. 放行
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        // 移除用户，防止内存泄漏
+        UserHolder.removeUser();
+    }
 }
