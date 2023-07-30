c74cdcd0a77ecaae8b0c07d69a5c10801d042dc8
程瑜 <2479676487@qq.com>
2023-07-30 14:55:13 +0800

    Feat: Add shop-list in Redis

--- src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
@@ -1,10 +1,17 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.ShopType;
 import com.hmdp.mapper.ShopTypeMapper;
 import com.hmdp.service.IShopTypeService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * <p>
@@ -17,4 +24,29 @@ import org.springframework.stereotype.Service;
 @Service
 public class ShopTypeServiceImpl extends ServiceImpl<ShopTypeMapper, ShopType> implements IShopTypeService {
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public List getTypeList() {
+
+        String key = "cache:shop_list";
+        // 1. 从 Redis 中查询商铺缓存
+        String shop_list = stringRedisTemplate.opsForValue().get(key);
+        // 2. 判断缓存是否命中
+        if (StrUtil.isNotBlank(shop_list)) {
+            // 3. 命中返回店铺信息
+            return JSONUtil.toList(shop_list, ShopType.class);
+        }
+        // 4. 未命中直接查询数据库
+        List<ShopType> shopTypeList = query().orderByAsc("sort").list();
+        // 4.1 不存在直接返回 404
+        if (shopTypeList.isEmpty()) {
+            return null;
+        }
+        // 4.2 存在则将列表存入 Redis
+        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shopTypeList));
+        // 5. 返回店铺信息
+        return shopTypeList;
+    }
 }
